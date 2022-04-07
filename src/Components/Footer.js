14290ade4e598a3cae3430b315f7d113f41477bf
CARLOS <carlos.huayamares.d@uni.pe>
2022-04-07 09:39:35 -0500

    second commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2022 Carlos Huayamares</li>
-              <li>Design by <a title="Styleshout" target="_blank" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
